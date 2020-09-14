6c4a251d2331728293ff44219600797009941157
Sierra Wallace <sskwallace@gmail.com>
2020-09-14 04:35:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,10 +70,11 @@ end
     if empty
       false
     WIN_COMBINATIONS.each do |combination|
-      elsif @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X" || @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O" 
+      if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X" || @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O" 
         return combination
       end
     end
     false
   end
 end
+
