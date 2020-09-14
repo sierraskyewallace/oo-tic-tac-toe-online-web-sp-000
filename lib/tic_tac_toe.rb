d4c4f4ac13255423a39afc5e12ce2ad7b0d92c9b
Sierra Wallace <sskwallace@gmail.com>
2020-09-14 04:52:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,20 +95,7 @@ end
   end
 end
   def winner 
-    WIN_COMBINATIONS.detect do |combo|
-      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
-        return "X"
-        elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-        return "O"
-      else
-        return nil
-      end
-    end
-  end
-  def play 
-    until over? 
-    turn
-  end
+    
   
   
 end
